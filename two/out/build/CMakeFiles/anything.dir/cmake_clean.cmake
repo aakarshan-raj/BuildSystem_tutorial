e8e774f2e1e046871e152fea182eaffba6fc1cf2
aakarshan-raj <aakarshan7raj@gmail.com>
2023-06-14 14:37:23 +0530

    started with cmakelists

---
+++ two/out/build/CMakeFiles/anything.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anything.dir/main.cpp.o"
+  "CMakeFiles/anything.dir/main.cpp.o.d"
+  "anything"
+  "anything.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/anything.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ two/out/build/CMakeFiles/anything.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for anything.
+# This may be replaced when dependencies are built.
